ca561fbb48b4fec1f1cff9f7d92ffa8271cbf82c
Kumar Piyush <krpiyush3@gmail.com>
2017-10-14 20:00:41 +0000

    Add files via upload

---
+++ Automate_game_2048.py
@@ -0,0 +1,25 @@
+import webbrowser
+import random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver=webdriver.Chrome()
+driver.get('https://gabrielecirulli.github.io/2048/')
+
+elem = driver.find_element_by_tag_name('body')
+while True:
+	elem.send_keys(Keys.DOWN)   # replace the (Arrow key) with your logic to score better:)
+	elem.send_keys(Keys.LEFT)
+	elem.send_keys(Keys.DOWN)    
+	elem.send_keys(Keys.RIGHT)
+	
+
+
+
+	
+
+
+
+	
